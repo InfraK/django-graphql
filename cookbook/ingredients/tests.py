c1394e16d73375ccb614fa05ede402dd08aad062
Santiago Kent <santiagokent@gmail.com>
2024-10-16 09:58:06 -0300

    test: add pytest dependencies and configuration, create a small simple test using fixtures

--- cookbook/ingredients/tests.py
+++ cookbook/ingredients/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+import json
+import pytest
+from graphene_django.utils.testing import graphql_query
 
-# Create your tests here.
+
+@pytest.fixture
+def client_query(client):
+    def func(*args, **kwargs):
+        return graphql_query(*args, **kwargs, client=client)
+
+    return func
+
+
+@pytest.mark.django_db
+def test_get_ingredients(client_query):
+    response = client_query(
+        """
+        query {
+            allIngredients {
+                edges {
+                    node {
+                        name
+                        id
+                        notes
+                    }
+                }
+            }
+        }
+        """,
+    )
+
+    content = json.loads(response.content)
+    print(content)
+    assert "errors" not in content
+    assert len(content["data"]["allIngredients"]["edges"]) == 4
+
